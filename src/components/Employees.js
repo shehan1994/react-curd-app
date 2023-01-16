b1da35435062e87391ba42e2e1d977cf0dbf8a76
shehan1994 <anushka.shehan94@gmail.com>
2023-01-16 14:04:16 +0530

    curd operations

---
+++ src/components/Employees.js
@@ -0,0 +1,19 @@
+const Employees = [
+    {
+        id: "1",
+        Name: "Shehan Anushka",
+        Age: "30"
+    },
+    {
+        id: "2",
+        Name: "DDS De Silva",
+        Age: "32"
+    },
+    {
+        id: "3",
+        Name: "Angelo Mathiws",
+        Age: "33"
+    },
+]
+
+export default Employees;
